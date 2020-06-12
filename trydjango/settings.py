9e6f7e9f8179da683f8686ba7fc6a4f09ebc2902
adit19shah <58650668+adit19shah@users.noreply.github.com>
2020-06-12 15:03:53 +0530

    Update settings.py

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -128,4 +128,4 @@ EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='visitormanager99@gmail.com'
-EMAIL_HOST_PASSWORD='aditasavisitor'
+EMAIL_HOST_PASSWORD='----------------------'
